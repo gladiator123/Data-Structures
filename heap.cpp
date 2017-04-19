abbce217eac48b86b6b97f1fbae9247e300dc7bf
gladiator123 <medhsupcom@gmail.com>
2017-04-19 21:34:26 +0200

    editing heap

--- heap.cpp
+++ heap.cpp
@@ -6,25 +6,49 @@ template<typename T>
 class Heap {
 
     vector<T> hp;
-
+    unordered_map<T,int> rank;
     public:
 
-    Heap(int sz): hp(sz) {}
+    Heap(int sz): hp(sz+1) {}
 
     void insert(const T elem){
+        if(rank.find(elem)==rank.end())return;
+
+        int index = rank[elem] = hp.size();
         hp.push_back(elem);
-        int par_index=(hp.size()-1)/2,cur_index=hp.size();
-        T& parent=hp[par_index];
-        while(parent<elem){
-            swap(hp[par_index],hp[cur_index]);
-            cur_index=par_index;
-            if(cur_index==0)break;
-            par_index=(hp.size()-1)/2;
+        up(index);
+    }
+    void up(int index){
+        while (index>1 && hp[index]>hp[index/2]){
+            swap(hp[index],hp[index/2]);
+            index/=2;
+        }  
+    }
+    void down(int index){
+        int left=index*2;
+        int right=left+1;
+        int n=length();
+        if(left<n && hp[left]>hp[index] && hp[left]>hp[right]){
+            swap(rank[left],rank[index]);
+            swap(hp[left],hp[index]);
+            down(left);
+        }else if(right<n && hp[right]>hp[index]){
+            swap(rank[right],rank[index]);
+            swap(hp[right],hp[index]);
+            down(right);
+        }else{
+            return;
         }
     }
+
     T& get_max(){
-        return hp[0];
+        if (length()>0) return hp[1];
     }
+
+    int length(){
+        return hp.size();
+    }
+
 
 
 } ;
